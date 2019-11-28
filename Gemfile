283b2fc675c10f6deca2b2274dd2322620f475ae
hoangtuyb96 <hoangtuyb96@gmail.com>
2019-11-28 16:25:19 +0900

    init_client

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem "bootsnap", ">= 1.1.0", require: false
 gem "devise"
 gem "faker"
 gem "simple_token_authentication", "~> 1.0"
+gem 'rack-cors', require: 'rack/cors'
 
 group :development, :test do
   # Call "byebug" anywhere in the code to stop execution and get a debugger console
---
+++ client/src/components/HomePagesContainer.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Container, Row, Col, Table } from 'reactstrap';
+
+class HomePagesContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      groups: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/home')
+    .then(response => {
+      console.log(response.data.data.groups.object)
+      this.setState({
+        groups: response.data.data.groups.object
+      })
+    })
+    .catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <div className="List_group">
+      <Container>
+        <h1 className="IssyoGarou-title">Groups</h1>
+        <Row>
+          <Table striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Created at</th>
+              </tr>
+            </thead>
+            <tbody>
+            {this.state.groups.map( group => {
+              return (
+                <tr key={group.id}>
+                  <td>{group.id}</td>
+                  <td>{group.name}</td>
+                  <td>{group.description}</td>
+                  <td>{group.created_at}</td>
+                </tr>
+              )
+            })}
+            </tbody>
+          </Table>
+        </Row>
+      </Container>
+      </div>
+    );
+  }
+}
+
+export default HomePagesContainer;
