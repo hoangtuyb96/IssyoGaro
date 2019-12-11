e418e25022212d464dda0910c58d8ef03830d1e7
hoangtuyb96 <hoangtuyb96@gmail.com>
2019-12-11 09:31:04 +0900

    fix_result_when_finish_goal

--- app/helpers/goal_helper.rb
+++ app/helpers/goal_helper.rb
@@ -3,13 +3,14 @@ module GoalHelper
   def serializer_goal(goal, cur_user_id)
     ug = UserGoal.search_role(cur_user_id, goal.id).take
     ugroup = UserGroup.search_role(cur_user_id, goal.group.id).take
+    achievements = Achievement.where(goal_id: goal.id)
     goal_serializer = {
       "id": goal.id,
       "name": goal.name,
       "description": goal.description,
       "start_day": custom_time(goal.start_day),
       "end_day": custom_time(goal.end_day),
-      "achievements": goal.achievements.blank? ? [] : Serializers::Achievements::AchievementSerializer.new(object: goal.achievements).serializer,
+      "achievements": achievements.blank? ? [] : Serializers::Achievements::AchievementSerializer.new(object: achievements).serializer,
       "is_joined": ug.present? ? true : false,
       "is_admin": ugroup.role.eql?(1) ? false : true,
       "user_goal_id": ug.present? ? ug.id : nil,
